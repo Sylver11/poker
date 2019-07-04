facd0e928f89903ab82ede75da604e03450df941
Sylver11 <justusvoigt@Admins-MacBook-Air.local>
2019-07-04 14:39:58 +0200

    throwing error saying no matching clause: 2

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -1,8 +1,15 @@
 (ns poker
   (:gen-class))
-[{:suit :diamond...}]
+
 
 (def deck
-  (for [value (range 0 14)
-        suite [:diamonds :hearts :clubs :spades]]
-    [value suite]))
+  (for [suite [:diamonds :hearts :clubs :spades]
+        value (range 1 14)]
+    [(case value
+       1 :Ace
+       11 :Jack
+       12 :Queen
+       13 :King)
+     suite]))
+
+
